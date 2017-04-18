21901add9648e4c9d853ecfd514b6e118071d4c1
Molly Brown <mollbrown@gmail.com>
2017-04-17 18:10:33 -0600

    confirm service is working with localhost

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,11 @@
 class Link
 
+  attr_reader :url
+
+  def initialize(attributes = {})
+    @url = attributes[:url]
+  end
+
   def self.hot_reads
     HotReadsService.hot_reads.map do |raw_link|
       new(raw_link)
--- app/services/hot_reads_service.rb
+++ app/services/hot_reads_service.rb
@@ -1,7 +1,7 @@
 class HotReadsService
 
   def self.hot_reads
-    response = Faraday.get("https://lit-citadel-12413.herokuapp.com/api/v1/hotreads")
+    response = Faraday.get("http://localhost:3000/api/v1/links/hotreads")
     JSON.parse(response.body, symbolize_names: true)
   end
 
