e324b85482310e190fc721a39398d38adf3463b9
Molly Brown <mollbrown@gmail.com>
2017-04-20 04:41:15 -0600

    add read count to link attributes

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,9 +1,10 @@
 class Link
 
-  attr_reader :url
+  attr_reader :url, :reads
 
   def initialize(attributes = {})
     @url = attributes[:url]
+    @reads = attributes[:read_count]
   end
 
   def self.hot_reads
